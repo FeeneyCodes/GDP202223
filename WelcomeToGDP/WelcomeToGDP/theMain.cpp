754464e611ee12dc993cf89ebe8f480e14604e30
michaelfeeney <michaelwcfeeney@gmail.com>
2022-09-08 13:56:08 -0400

    VERY exciting coding

---
+++ WelcomeToGDP/WelcomeToGDP/theMain.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <string>
+//#include <fstream>
+//#include <sstream>
+
+int main( int argc, char* argv[] )
+{
+
+	std::cout << "Hello GDP'er!" << std::endl;
+
+	int x = 0;
+	std::cin >> x;		// "Michael"
+	std::string MyName;
+	std::cin >> MyName;
+
+//	myFile >> myString;
+//	ssHey << "a";
+
+
+	return 0;
+}
