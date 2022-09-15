6ca8977ebf3c903e0e07a53236cf1988d11f37f2
michaelfeeney <michaelwcfeeney@gmail.com>
2022-09-15 15:50:48 -0400

    Lots of triangles

--- ILoveOpenGL/ILoveOpenGL/theMainFunction.cpp
+++ ILoveOpenGL/ILoveOpenGL/theMainFunction.cpp
@@ -14,27 +14,45 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>  // "string builder" type thing
 
-static const struct
+struct sVertex_XY_RGB
 {
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-{
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+    float x, y, z;             // 0 floats from the start
+    float r, g, b;          // 2 floats from the start
+                     // Each vertex is 5 floats in size
 };
+
+//sVertex_XY_RGB vertices[] =
+//{
+//        { -0.6f, -0.4f, 0.0, 1.0f, 0.0f, 0.0f },
+//        {  0.6f, -0.4f, 0.0, 0.0f, 1.0f, 0.0f },
+//        {  0.0f,  0.6f, 0.0, 0.0f, 0.0f, 1.0f },
+//
+//        { (-0.6f + 1.0f), -0.4f, 0.0,  0.0f, 1.0f, 0.0f },
+//        {  (0.6f + 1.0f), -0.4f, 0.0,  0.0f, 0.0f, 1.0f },
+//        {  (0.0f + 1.0f),  0.6f, 0.0,  1.0f, 0.0f, 0.0f }
+//};
+
+const unsigned int NUMBER_OF_VERTICES = 100;
+
+sVertex_XY_RGB* vertices = new sVertex_XY_RGB[NUMBER_OF_VERTICES];
+
+glm::vec3 g_cameraEye = glm::vec3(0.0, 0.0, -4.0f);
+glm::vec3 g_cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+
 
 static const char* vertex_shader_text =
 "#version 110\n"
 "uniform mat4 MVP;\n"
 "attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
+"attribute vec3 vPos;\n"
 "varying vec3 color;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    gl_Position = MVP * vec4(vPos, 1.0);\n"
 "    color = vCol;\n"
 "}\n";
 
@@ -51,14 +69,74 @@ static void error_callback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void key_callback(GLFWwindow* window, 
+                         int key, int scancode, 
+                         int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+
+    // Move the camera
+    // AWSD   AD - left and right
+    //        WS - forward and back
+    const float CAMERA_MOVE_SPEED = 0.1f;
+    if (key == GLFW_KEY_A )     // Left
+    {
+        ::g_cameraEye.x -= CAMERA_MOVE_SPEED;
+    }
+    if (key == GLFW_KEY_D )     // Right
+    {
+        ::g_cameraEye.x += CAMERA_MOVE_SPEED;
+    }
+    if (key == GLFW_KEY_W )     // Forward
+    {
+        ::g_cameraEye.z += CAMERA_MOVE_SPEED;
+    }
+    if (key == GLFW_KEY_S )     // Backwards
+    {
+        ::g_cameraEye.z -= CAMERA_MOVE_SPEED;
+    }
+    if (key == GLFW_KEY_Q )     // Down
+    {
+        ::g_cameraEye.y -= CAMERA_MOVE_SPEED;
+    }
+    if (key == GLFW_KEY_E )     // Up
+    {
+        ::g_cameraEye.y += CAMERA_MOVE_SPEED;
+    }
+
+    if ( key == GLFW_KEY_1 )
+    {
+        ::g_cameraEye = glm::vec3(-5.5f, -3.4f, 15.0f); 
+    }
+
+}
+// From here: https://stackoverflow.com/questions/5289613/generate-random-float-between-two-floats/5289624
+float RandomFloat(float a, float b) {
+    float random = ((float)rand()) / (float)RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
 }
 
 int main(void)
 {
+    // Create a bunch of random triangles
+    for ( unsigned int index = 0; index != NUMBER_OF_VERTICES; index++ )
+    {
+        vertices[index].x = RandomFloat(-50.0f, 50.0f);
+        vertices[index].y = RandomFloat(-50.0f, 50.0f);
+        vertices[index].z = RandomFloat(-50.0f, 50.0f);
+
+        vertices[index].r = RandomFloat(0.0f, 1.0f);
+        vertices[index].g = RandomFloat(0.0f, 1.0f);
+        vertices[index].b = RandomFloat(0.0f, 1.0f);
+    }
+
+    std::cout << "starting up..." << std::endl;
+
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
@@ -71,12 +149,14 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello Kitty", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+
+    std::cout << "Window created." << std::endl;
 
     glfwSetKeyCallback(window, key_callback);
 
@@ -87,10 +167,13 @@ int main(void)
 
     // NOTE: OpenGL error checks have been omitted for brevity
 
+    // Defining the 3D model that we are going to draw. 
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * NUMBER_OF_VERTICES, vertices, GL_STATIC_DRAW);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // Shader setup
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
@@ -100,22 +183,37 @@ int main(void)
     glCompileShader(fragment_shader);
 
     program = glCreateProgram();
+
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
+
     glLinkProgram(program);
 
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
+    // Vertex layout
+    vpos_location = glGetAttribLocation(program, "vPos");   // Vertex Position
     vcol_location = glGetAttribLocation(program, "vCol");
 
     glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+    glVertexAttribPointer(vpos_location,
+                          3,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          sizeof(sVertex_XY_RGB),   //  sizeof(vertices[0]),
+                          (void*) offsetof(sVertex_XY_RGB, x));      //0);
 
-    while (!glfwWindowShouldClose(window))
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 
+                          3, 
+                          GL_FLOAT, 
+                          GL_FALSE,
+                          sizeof(sVertex_XY_RGB),               //  sizeof(vertices[0]),
+                          (void*) offsetof(sVertex_XY_RGB, r)); //  (sizeof(float) * 3));
+
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+
+
+    while ( ! glfwWindowShouldClose(window) )
     {
         float ratio;
         int width, height;
@@ -141,23 +239,45 @@ int main(void)
 
         glm::mat4x4 v = glm::mat4(1.0f);
 
-        glm::vec3 cameraEye = glm::vec3(0.0, 0.0, -4.0f);
-        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        //glm::vec3 cameraEye = glm::vec3(0.0, 0.0, -4.0f);
+        //glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
         glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
 
-        v = glm::lookAt(cameraEye,
-                        cameraTarget,
+        v = glm::lookAt(::g_cameraEye,
+                        ::g_cameraTarget,
                         upVector);
 
 //        mat4x4_mul(mvp, p, m);
         mvp = p * v * m; 
 
+
         glUseProgram(program);
+
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glPointSize(15.0f);
+        glDrawArrays(GL_TRIANGLES, 0, NUMBER_OF_VERTICES);
+//        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        // Set the window title
+        //glfwSetWindowTitle(window, "Hey");
+
+        std::stringstream ssTitle;
+        ssTitle << "Camera (x,y,z): "
+            << ::g_cameraEye.x << ", "
+            << ::g_cameraEye.y << ", "
+            << ::g_cameraEye.z;
+
+        std::string theText = ssTitle.str();
+
+        glfwSetWindowTitle(window, ssTitle.str().c_str() );
+        // Or this:
+        //std::string theText = ssTitle.str();
+        //glfwSetWindowTitle(window, ssTitle.str().c_str() );
+
     }
 
     glfwDestroyWindow(window);
