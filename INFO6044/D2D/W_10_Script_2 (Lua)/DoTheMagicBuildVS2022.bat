2f7c4351de873b1fc5840e4c3ad515e4787e89e3
michaelfeeney <michaelwcfeeney@gmail.com>
2022-12-01 12:10:14 -0500

    Added Lua stuff

---
+++ INFO6044/D2D/W_10_Script_2 (Lua)/DoTheMagicBuildVS2022.bat
@@ -0,0 +1,9 @@
+cl /MD /O2 /c /DLUA_BUILD_AS_DLL *.c
+ren lua.obj lua.o
+ren luac.obj luac.o
+link /DLL /IMPLIB:lua5.4.4.lib /OUT:lua5.4.4.dll *.obj
+link /OUT:lua.exe lua.o lua5.4.4.lib
+lib /OUT:lua5.4.4-static.lib *.obj
+link /OUT:luac.exe luac.o lua5.4.4-static.lib
+@echo All done.
+pause
